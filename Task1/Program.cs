c2ab27535c50843a2aad2f03a62ebacb3e032cc5
Max Vasylenko <13havikon@gmail.com>
2022-11-12 00:34:59 +0200

    Сhanged the text in three lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("На курсах ми вивчаємо як створювати гілки та перемикатись між ними");
+Console.WriteLine("На курсах ми вивчаємо як створювати гілки та перемикатись між ними");
+Console.WriteLine("На курсах ми вивчаємо як створювати гілки та перемикатись між ними");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
